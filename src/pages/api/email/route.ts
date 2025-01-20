55740ef985d3b6b785ce0030076991eab32ef831
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2025-01-21 00:16:14 +0300

    Try to edit api handler function

--- src/pages/api/email/route.ts
+++ src/pages/api/email/route.ts
@@ -1,38 +1,68 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const SMPT_HOST = process.env.SMTP_SERVER_HOST;
 const SMPT_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMPT_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
 
-export default async function POST(request: Request) {
-  try {
-    const { name, email, message } = await request.json();
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, message, name } = JSON.parse(req.body);
 
-    const transporter = nodemailer.createTransport({
-      host: SMPT_HOST,
-      port: 465,
-      secure: true,
-      auth: {
-        SMPT_USERNAME,
-        SMPT_PASSWORD,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    host: SMPT_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      SMPT_USERNAME,
+      SMPT_PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-      from: SMPT_USERNAME,
-      to: email,
-      subject: "ПРИЗНАНИЕ В ЛЮБВИ",
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
+  const mailOptions = {
+    from: SMPT_USERNAME,
+    to: email,
+    subject: "ПРИЗНАНИЕ В ЛЮБВИ",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
-    await transporter.sendMail(mailOptions);
+  transporter.sendMail(mailOptions);
 
-    return NextResponse.json(
-      { message: "Message sent successfully" },
-      { status: 200 },
-    );
-  } catch (error) {
-    return new NextResponse("Failed to send message.", { status: 500 })
-  }
+  res.status(200)
+
 }
+
+// export default async function POST(request: Request) {
+//   try {
+//     const { name, email, message } = await request.json();
+//
+//     const transporter = nodemailer.createTransport({
+//       host: SMPT_HOST,
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         SMPT_USERNAME,
+//         SMPT_PASSWORD,
+//       },
+//     });
+//
+//     const mailOptions = {
+//       from: SMPT_USERNAME,
+//       to: email,
+//       subject: "ПРИЗНАНИЕ В ЛЮБВИ",
+//       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+//     };
+//
+//     await transporter.sendMail(mailOptions);
+//
+//     return NextResponse.json(
+//       { message: "Message sent successfully" },
+//       { status: 200 },
+//     );
+//   } catch (error) {
+//     return new NextResponse("Failed to send message.", { status: 500 })
+//   }
+// }
